9c8943f29a0b4b82832c6a25db77e86a40887877
Mathieu Mitchell <mmitchell@iweb.com>
2015-08-13 15:22:26 -0400

    redlock 2.0.0

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ THE SOFTWARE.
 
 setup(
     name='redlock-py',
-    version='1.0.5',
+    version='2.0.0',
     packages=find_packages(),
     include_package_data=True,
     description='Redis locking mechanism',
